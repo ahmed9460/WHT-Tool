12ce934f3a06dd017447c4daede0f3ae86190c92
E4NF-WHT <62159951+mohamed708@users.noreply.github.com>
2020-04-25 17:49:47 +0200

    Update WHT-Tool.py

--- WHT-Tool.py
+++ WHT-Tool.py
@@ -50,23 +50,20 @@ if E4 == 1:
     "10":"git clone https://github.com/rajkumardusad/Tool-X.git"
     "11":"git clone https://github.com/emre/fb.py.git"  
     "12":"git clone https://github.com/Vairous7x/V7x-Fishing2.git"
-    "13":"git clone https://github.com/No-Name-404/N404-Tool.git"  
+    "13":"git clone https://github.com/No-Name-404/N404-Tool.git"
+    "14":"git clone https://github.com/beefproject/beef.git"
     }
     #######################################################################
     style ="""
-    [1]Lazymux
-    [2]Spammer-Grab
-    [3]ngrok
-    [4]hammer
-    [5]hulk
-    [6]wifite
-    [7]EasY_HaCk
-    [8]wifi-scanner
-    [9]sqlmap
-    [10]Tool-X
-    [11]fb
-    [12]V7x-Fishing2
-    [13]N404-Tool
+    [1]Lazymux              [2]Spammer-Grab         [3]ngrok   
+  
+    [4]hammer               [5]hulk                 [6]wifite
+                  
+    [7]EasY_HaCk            [8]wifi-scanner         [9]sqlmap
+    
+    [10]Tool-X              [11]fb                  [12]V7x-Fishing2
+    
+    [13]N404-Tool           [14]BeeF-TooL
     """
     #######################################################################
     import os
@@ -87,10 +84,10 @@ if E4 == 2:
     "4":"git clone https://github.com/jseidl/GoldenEye.git"
   }
   style_d = """
-  [1]hammer
-  [2]hulk
-  [3]DDos-Attack
-  [4]ip location
+  [1]hammer              [2]hulk   
+  
+  [3]DDos-Attack         [4]Golden-EYE
+  
   """
   import os
   installer_dos(numberd):
@@ -102,12 +99,28 @@ if E4 == 2:
     user1 = input("enter number tool: ")
     installer_dos(user1)
 ######################################################################
-#if E4 = 6:
-  #t_in = {
-    #"1":"git clone https://github.com/nmap/nmap.git"
-    #"2":"git clone https://github.com/maldevel/IPGeoLocation.git"
-    #"3":"git clone https://github.com/shawarkhanethicalhacker/D-TECT-1.git"
-  #}
+if E4 = 6:
+  t_in = {
+    "1":"git clone https://github.com/nmap/nmap.git"
+    "2":"git clone https://github.com/maldevel/IPGeoLocation.git"
+    "3":"git clone https://github.com/shawarkhanethicalhacker/D-TECT-1.git"
+    "4":"git clone https://github.com/UndeadSec/EvilURL.git"
+  }
+  style_in = """
+  [1]nmap              [2]ip location
+  
+  [3]D-TECT            [4]EVIL-URL
+  
+  """
+  import os
+  def installer_inf(numberi):
+    try:
+      os.system(t_in[numberi])
+    except:
+      print("error 404 not found....")
+    print(style_in)  
+    user2 = input("enter numbrt tool: ")
+    installer_inf(user2)
 #######################################################################
 elif E4 == 7:
     print(""">>>>>>>>>>>welcome to in my tool<<<<<<<<<<<<<<<
