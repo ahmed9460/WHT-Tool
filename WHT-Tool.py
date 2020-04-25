a817f5c1d7db026cd621c8a241f88ff0d990e546
mohamed708 <62159951+mohamed708@users.noreply.github.com>
2020-04-25 02:38:06 +0200

    Update WHT-Tool.py

--- WHT-Tool.py
+++ WHT-Tool.py
@@ -24,7 +24,7 @@ print('                                                                     ')
 
 import enter_user
 ############################################################################3
-style_tool= """                         1-Tool-Termux              2-Dos attack
+style_tool= """                              1-Tool-Termux              2-Dos attack
 
                               3-fishing                  4-scaning
 
@@ -39,34 +39,32 @@ if E4 == 1:
     time.sleep(3)
     tools = {
     "1":"git clone https://github.com/Gameye98/Lazymux.git"
-    "2":"git clone https://github.com/Vairous7x/V7x-Tool.git"
-    "3":"git clone https://github.com/p4kl0nc4t/Spammer-Grab.git"
-    "4":"git clone https://github.com/inconshreveable/ngrok.git"
-    "5":"git clone https://github.com/cyweb/hammer.git"
-    "6":"git clone https://github.com/grafov/hulk.git"
-    "7":"git clone https://github.com/derv82/wifite.git"
-    "8":"git clone https://github.com/sabri-zaki/EasY_HaCk.git"
-    "9":"git clone https://github.com/israelbuitron/wifi-scanner.git"
-    "10":"git clone https://github.com/sqlmapproject/sqlmap.git"
-    "11":"git clone https://github.com/rajkumardusad/Tool-X.git"
-    "12":"git clone https://github.com/emre/fb.py.git"  
-    "13":"git clone https://github.com/Vairous7x/V7x-Fishing2.git"
+    "2":"git clone https://github.com/p4kl0nc4t/Spammer-Grab.git"
+    "3":"git clone https://github.com/inconshreveable/ngrok.git"
+    "4":"git clone https://github.com/cyweb/hammer.git"
+    "5":"git clone https://github.com/grafov/hulk.git"
+    "6":"git clone https://github.com/derv82/wifite.git"
+    "7":"git clone https://github.com/sabri-zaki/EasY_HaCk.git"
+    "8":"git clone https://github.com/israelbuitron/wifi-scanner.git"
+    "9":"git clone https://github.com/sqlmapproject/sqlmap.git"
+    "10":"git clone https://github.com/rajkumardusad/Tool-X.git"
+    "11":"git clone https://github.com/emre/fb.py.git"  
+    "12":"git clone https://github.com/Vairous7x/V7x-Fishing2.git"
     }
     #######################################################################
     style ="""
     [1]Lazymux
-    [2]V7x-Tool
-    [3]Spammer-Grab
-    [4]ngrok
-    [5]hammer
-    [6]hulk
-    [7]wifite
-    [8]EasY_HaCk
-    [9]wifi-scanner
-    [10]sqlmap
-    [11]Tool-X
-    [12]fb
-    [13]V7x-Fishing2
+    [2]Spammer-Grab
+    [3]ngrok
+    [4]hammer
+    [5]hulk
+    [6]wifite
+    [7]EasY_HaCk
+    [8]wifi-scanner
+    [9]sqlmap
+    [10]Tool-X
+    [11]fb
+    [12]V7x-Fishing2
     """
     #######################################################################
     import os
