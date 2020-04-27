f2d2771cc06a725cf0db77156950ea2ca6bb83a8
E4NF-WHT <62159951+mohamed708@users.noreply.github.com>
2020-04-27 06:28:37 +0200

    Add files via upload

---
+++ enter.py
@@ -0,0 +1,8 @@
+
+password = str(input("please enter password:"))
+if password == 'E4NF':
+    print("done...")
+else:
+    print('error..')
+    exit()
+    
---
+++ style_tool.py
@@ -0,0 +1,10 @@
+
+style_tool = """                         [1]TooL-Termux                  [2]Dos atack
+
+                         [3]scaning(Everything)          [4]info Gathering
+
+                         [5]install metasploit           [6]about me
+
+                                             [7]exit"""
+
+print(style_tool)
