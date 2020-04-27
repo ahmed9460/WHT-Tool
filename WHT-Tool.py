e6950689e06f2e83e201e8dba990d5405467c127
E4NF-WHT <62159951+mohamed708@users.noreply.github.com>
2020-04-27 02:44:40 +0200

    Update WHT-Tool.py

--- WHT-Tool.py
+++ WHT-Tool.py
@@ -28,11 +28,51 @@ if E4 == 'E4NF':
   print('done....')
 else:
   print("error 404 not found...")
-style_tool= """                              1-Tool-Termux              2-Dos attack
-
-                              3-fishing                  4-scaning
-
-                              5-install-metasploit       6-info gathering
-
-                              7-about me                 8-exit"""
+exit()
+style_tool = """
+                      [1]tool-Termux
+"""
 print(style_tool)
+n_tool = int(input("enter nmber:"))
+if n_tool == 1:
+  tools = {
+  '1':'git clone https://github.com/Gameye98/Lazymux.git'
+  '2':'git clone https://github.com/p4kl0nc4t/Spammer-Grab.git'
+  '3':'git clone https://github.com/inconshreveable/ngrok.git'
+  '4':'git clone https://github.com/cyweb/hammer.git'
+  '5':'git clone https://github.com/grafov/hulk.git'
+  '6':'git clone https://github.com/derv82/wifite.git'
+  '7':'git clone https://github.com/sabri-zaki/EasY_HaCk.git'
+  '8':'git clone https://github.com/israelbuitron/wifi-scanner.git'
+  '9':'git clone https://github.com/sqlmapproject/sqlmap.git'
+  '10':'git clone https://github.com/rajkumardusad/Tool-X.git'
+  '11':'git clone https://github.com/emre/fb.py.git'
+  '12':'git clone https://github.com/Vairous7x/V7x-Fishing2.git'
+  '13':'git clone https://github.com/No-Name-404/N404-Tool.git'
+  '14':'git clone https://github.com/beefproject/beef.git'
+  }
+  style ="""
+  [1]Lazymux
+  [2]Spammer-Grab
+  [3]ngrok
+  [4]hammer
+  [5]hulk
+  [6]wifite
+  [7]EasY_HaCk
+  [8]wifi-scanner
+  [9]sqlmap
+  [10]Tool-X
+  [11]fb
+  [12]V7x-Fishing2
+  [13]N404-Tool
+  [14]beef
+  """
+  import os
+  def installer(number):
+    try:
+      os.system(tools[number])
+    except:
+      print("unknow...")
+    print(style)  
+    user = input("enter number tool:")
+    installer(user)
