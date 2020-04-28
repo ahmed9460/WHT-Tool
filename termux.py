4ab9e5dea06d48572c1e3de9da0082e89ae9c23a
E4NF-WHT <62159951+mohamed708@users.noreply.github.com>
2020-04-28 04:59:53 +0200

    Add files via upload

---
+++ termux.py
@@ -0,0 +1,18 @@
+style_termux = """
+             [1]Lazymux                [2]Spammer-Grab
+
+             [3]ngrok                  [4]hammer
+
+             [5]hulk                   [6]wifite
+
+             [7]EasY_HaCk              [8]wifi-scanner
+
+             [9]sqlmap                 [10]Tool-X
+
+             [11]fb                    [12]V7x-Fishing2
+
+             [13]V7x-Tool              [14]N404-Tool
+
+             [15]beef                  [16]fedora
+             """
+print(style_termux)
